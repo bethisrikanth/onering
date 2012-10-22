b33933d040e324c46dfd0cec30669bc3aa48407a
Ran Tavory <rantav@gmail.com>
2012-10-22 17:03:57 +0200

    Set up a default view view accept type of text/html from a basic haml template

--- plugins/core/init.rb
+++ plugins/core/init.rb
@@ -2,13 +2,22 @@ require 'controller'
 
 module App
   class Base < Controller
-    get '/' do 
+    set :haml, :format => :html5
+
+    get '/', :provides => 'html' do
+      pass unless request.accept.include? "text/html"
+      haml :index
+    end
+
+    get '/', :provides => 'json' do 
       {
         :status => 'ok',
         :local_root => PROJECT_ROOT,
         :environment => settings.environment
       }.to_json
     end
+
+
 
     get '/doc/routes' do
       rv = App::Base.routes['GET'].collect{|i| i.first }
