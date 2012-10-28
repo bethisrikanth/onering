8ea7ce69a9e565e60c2ab02d7427a06c41a62af5
Ran Tavory <rantav@gmail.com>
2012-10-28 12:17:44 +0200

    Add some unit tests to validate a device

--- spec/plugins/assets/models/device_spec.rb
+++ spec/plugins/assets/models/device_spec.rb
@@ -8,4 +8,19 @@ describe "device"  do
     @device.name = "xxx"
     @device.name.should == "xxx"
   end
+
+  describe "validations" do
+    it "empty id should not be valid " do
+      @device.id = ""
+      @device.should_not be_valid
+    end
+    it "short id should not be valid " do
+      @device.id = "123"
+      @device.should_not be_valid
+    end
+    it "6 hex id should be valid " do
+      @device.id = "123abc"
+      @device.should be_valid
+    end
+  end
 end
