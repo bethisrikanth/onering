e9cc92ab9e683c040e8497f5bcf346ccd55f1fa6
Ran Tavory <rantav@gmail.com>
2012-10-29 12:22:57 +0200

    Actually add the generated specs

---
+++ spec/javascripts/_gen/controllersSpec.js
@@ -0,0 +1,54 @@
+(function() {
+
+  describe('DefaultController', function() {
+    var controller;
+    controller = null;
+    beforeEach(function() {
+      return controller = new DefaultController();
+    });
+    return it('should not be null', function() {
+      return expect(controller).not.toBe(null);
+    });
+  });
+
+  describe('QueryController', function() {
+    var data, http, queryController, route, routeParams, scope;
+    queryController = scope = http = route = routeParams = data = null;
+    beforeEach(inject(function($controller) {
+      scope = {};
+      routeParams = {
+        field: 'f',
+        query: 'q'
+      };
+      route = {
+        current: {
+          $route: {
+            params: {}
+          }
+        }
+      };
+      data = '123';
+      http = jasmine.createSpy('http').andReturn({
+        success: function(callback) {
+          return callback(data);
+        }
+      });
+      return queryController = $controller('QueryController', {
+        $scope: scope,
+        $http: http,
+        $route: route,
+        $routeParams: routeParams
+      });
+    }));
+    it('should call the API /devices/find/', function() {
+      return expect(http).toHaveBeenCalledWith({
+        method: 'GET',
+        url: "/devices/find/" + scope.field + "/" + scope.query
+      });
+    });
+    return it('should attach the devices to the $scope', function() {
+      return expect(scope.devices).toEqual(data);
+    });
+  });
+
+}).call(this);
