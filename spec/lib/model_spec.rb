b49ba024decfa5629627867b174790012d6c0c02
Ran Tavory <rantav@gmail.com>
2012-11-05 10:30:05 +0200

    Add test for to_json

--- spec/lib/model_spec.rb
+++ spec/lib/model_spec.rb
@@ -10,10 +10,19 @@ describe "App::Model"  do
       it "new object should only to_h it's id" do
         @m.to_h.should == {"id" => @m.id}
       end
-      it "the property _type should not be serializble" do
+      it "the property _type should not be serializble, but all others should" do
         @m["_type"] = 5
         @m["x"] = 7
         @m.to_h.should == {"id" => @m.id, "x" => 7}
+      end
+    end
+    describe "to_json" do
+      before :each do
+        @m = TestModel.new
+      end
+      it "should make a json string" do
+        @m["x"] = 7
+        @m.to_json.should == {"id" => @m.id, "x" => 7}.to_json
       end
     end
   end
