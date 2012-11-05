f75612b7e0290c9961160492d30fab547f1ac962
Ran Tavory <rantav@gmail.com>
2012-11-05 10:24:31 +0200

    Add more coverage to to_h

--- spec/lib/model_spec.rb
+++ spec/lib/model_spec.rb
@@ -10,6 +10,11 @@ describe "App::Model"  do
       it "new object should only to_h it's id" do
         @m.to_h.should == {"id" => @m.id}
       end
+      it "the property _type should not be serializble" do
+        @m["_type"] = 5
+        @m["x"] = 7
+        @m.to_h.should == {"id" => @m.id, "x" => 7}
+      end
     end
   end
 end
