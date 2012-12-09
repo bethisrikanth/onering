9e280270a4ad517769396613da48eecf083ad43d
Gary Hetzel <garyhetzel@gmail.com>
2012-12-09 01:20:52 -0500

    Added auto-reload to navbar OKAY I'M GOING TO BED NOW

--- plugins/core/public/js/controllers.js
+++ plugins/core/public/js/controllers.js
@@ -2,27 +2,32 @@ function DefaultController($scope){
 
 }
 
-function NavigationController($scope, $http, $route, $routeParams, Summary){
-//get site summary
-  Summary.query({
-    field: 'site'
-  }, function(data){
-    $scope.sites = data;
-  });
-
-//get status summary
-  Summary.query({
-    field: 'status'
-  }, function(data){
-    $scope.statuses = data;
-  });
-
-//get alert state summary
-  Summary.query({
-    field: 'alert_state'
-  }, function(data){
-    $scope.alert_states = $.grep(data, function(el){
-      return (el.id !== null);
+function NavigationController($scope, $http, $route, $window, $routeParams, Summary){
+  $scope.reload = function(){
+  //get site summary
+    Summary.query({
+      field: 'site'
+    }, function(data){
+      $scope.sites = data;
     });
-  });
+
+  //get status summary
+    Summary.query({
+      field: 'status'
+    }, function(data){
+      $scope.statuses = data;
+    });
+
+  //get alert state summary
+    Summary.query({
+      field: 'alert_state'
+    }, function(data){
+      $scope.alert_states = $.grep(data, function(el){
+        return (el.id !== null);
+      });
+    });
+  }
+
+  $window.setInterval($scope.reload, 30000);
+  $scope.reload();
 }
