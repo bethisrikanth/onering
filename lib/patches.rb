d7226b4fbb287ee37882013c17efd249ae2cb817
Gary Hetzel <garyhetzel@gmail.com>
2013-01-24 12:17:50 -0500

    Hacky Net::HTTP timeout override

--- lib/patches.rb
+++ lib/patches.rb
@@ -1,4 +1,16 @@
 require 'deep_merge/rails_compat'
+require 'net/http'
+
+module Net
+  class HTTP
+    alias old_initialize initialize
+
+    def initialize(*args)
+        old_initialize(*args)
+        @read_timeout = 10
+    end
+  end
+end
 
 class String
   def underscore
