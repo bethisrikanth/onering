94843e8ed91aa6a8320fee759aeda22aa8b90255
Gary Hetzel <garyhetzel@gmail.com>
2013-05-02 18:27:51 -0400

    Updating user management screen

--- plugins/auth/public/js/controllers.js
+++ plugins/auth/public/js/controllers.js
@@ -81,134 +81,10 @@ function UserProfileController($scope, $http, $dialog, CurrentUser){
 }
 
 
-function UserManagerController($scope, $http, User, UserType, UserList, GroupList, CapabilityList){
-//USERS
-  $scope.submitting = false;
-  $scope._userClass = 'span12';
-
-  $scope.toggleUserPane = function(user){
-    if(user){
-      if($scope.userPaneUser && ($scope.userPaneUser.id == user.id)){
-    //  same user triggered, hide the pane
-        $scope._userClass = 'span12';
-        $scope._userPaneClass = 'hide';
-        $scope.userPaneUser = null;
-      }else{
-    //  different (or new) user, show pane and update
-        $scope._userClass = 'span4';
-        $scope._userPaneClass = 'span8';
-        $scope.userPaneUser = user;
-      }
-    }
-  }
-
-
-//GROUPS
-  $scope._groupClass = 'span12';
-
-  $scope.toggleGroupPane = function(group){
-    if(group){
-      if($scope.groupPaneGroup && ($scope.groupPaneGroup.id == group.id)){
-    //  same group triggered, hide the pane
-        $scope._groupClass = 'span12';
-        $scope._groupPaneClass = 'hide';
-        $scope.groupPaneGroup = null;
-      }else{
-    //  different (or new) group, show pane and update
-        $scope._groupClass = 'span4';
-        $scope._groupPaneClass = 'span8';
-        $scope.groupPaneGroup = group;
-      }
-    }
-  }
-
-
-//CAPABILITIES
-  $scope._capabilityClass = 'span12';
-
-  $scope.toggleCapabilityPane = function(capability){
-    if(capability){
-      if($scope.capabilityPaneCapability && ($scope.capabilityPaneCapability.id == capability.id)){
-    //  same capability triggered, hide the pane
-        $scope._capabilityClass = 'span12';
-        $scope._capabilityPaneClass = 'hide';
-        $scope.capabilityPaneCapability = null;
-      }else{
-    //  different (or new) capability, show pane and update
-        $scope._capabilityClass = 'span4';
-        $scope._capabilityPaneClass = 'span8';
-        $scope.capabilityPaneCapability = group;
-      }
-    }
-  }
-
-
-//operations
-  $scope.saveUser = function(user){
-    if(user){
-      $scope.submitting = true;
-
-      User.save({
-        user: user.id
-      }, user, function(u){
-    //  user object saved, check for change in type
-    //  if type changed, save it then reload
-        if(user.type != u.type){
-          UserType.get({
-            user: user.id,
-            type: user.type
-          }, function(){
-            $scope.submitting = false;
-            $scope.reload();
-          });
-
-    //  otherwise just reload
-        }else{
-          $scope.submitting = false;
-          $scope.reload();
-        }
-      });
-    }
-  }
-
-  $scope.addUserToGroup = function(user, group){
-    if(user && group){
-      $http.get('/api/groups/'+group+'/add/'+user).success(function(){
-        $scope.reload();
-      });
-    }
-  }
-
-  $scope.removeUserFromGroup = function(user, group){
-    if(user && group){
-      $http.get('/api/groups/'+group+'/remove/'+user).success(function(){
-        $scope.reload();
-      });
-    }
-  }
-
-  $scope.grantCapability = function(capability, type, id){
-    if(capability && type && id){
-    // grant
-    }
-  }
-
-  $scope.revokeCapability = function(capability, type, id){
-    if(capability && type && id){
-    // revoke
-    }
-  }
-
-
-//reloaders
+function UserManagerController($scope, $http, $dialog, User, UserType, UserList, GroupList, CapabilityList){
   $scope.reloadUsers = function(){
     UserList.query({}, function(data){
       $scope.users = data;
-      if($scope.userPaneUser){
-        $scope.userPaneUser = $.grep($scope.users, function(n){
-          return (n.id == $scope.userPaneUser.id);
-        })[0];
-      }
     });
   }
 
@@ -218,28 +94,38 @@ function UserManagerController($scope, $http, User, UserType, UserList, GroupLis
     });
   }
 
-  $scope.reloadCapabilities = function(){
-    CapabilityList.query({}, function(data){
-      $scope.capabilities = data;
-    });
-  }
-
   $scope.reload = function(){
     $scope.reloadUsers();
     $scope.reloadGroups();
-    $scope.reloadCapabilities();
-
-    $http.get('/api/users/list/types').success(function(data){
-      $scope.userTypes = [];
-
-      angular.forEach(data, function(i){
-        $scope.userTypes.push({
-          value: i,
-          label: i.replace('User','').titleize()
-        });
-      })
-    })
   }
+
+  $scope.userDialog = function(user){
+    var $parent = $scope;
+
+    var d = $dialog.dialog({
+      backdrop:    true,
+      keyboard:    true,
+      templateUrl: 'userDialog',
+      controller:  function($scope, $dialog, User){
+        $scope.user = user;
+        $scope.groups = $parent.groups;
+
+        $scope.reload = function(){
+          User.get({
+            id: $scope.user.id
+          }, function(data){
+            $scope.user = data;
+          });
+        }
+
+        $scope.close = function(){
+          d.close()
+        };
+      }
+    });
+
+    d.open()
+  };
 
   $scope.reload();
 }
