4c1d8536f2272a80bce51ad8427825e7e0779abc
Gary Hetzel <garyhetzel@gmail.com>
2013-07-09 11:07:32 -0400

    Robustified the rundeck node sync endpoint

--- plugins/rundeck/init.rb
+++ plugins/rundeck/init.rb
@@ -11,15 +11,23 @@ module App
         content_type 'text/x-yaml'
 
         return YAML.dump(nodes.collect{|node|
-          {
+          rv = {
             'nodename'  => node.get('rundeck.name', node.id),
             'hostname'  => (node.get(Config.get('automation.rundeck.fields.hostname') || 'fqdn') || node.name || node.id),
-            'username'  => node.get('rundeck.user', Config.get('automation.rundeck.user', 'rundeck')),
+            'username'  => (params[:username] || node.get('rundeck.user', Config.get('automation.rundeck.user', 'rundeck'))),
             'tags'      => node.tags,
             'osVersion' => node.get('version'),
             'osName'    => node.get('distro'),
             'osArch'    => node.get('arch')
-          }.compact
+          }
+	
+          Config.get('automation.rundeck.fields',{}).each do |field, value|
+            next if field.to_s == 'hostname'
+            value = node.get(value)
+            rv[field] = value
+          end
+ 
+          rv.compact
         })
       end
     end
