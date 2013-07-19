dc37b690e946178b4511f720ec9aba5abad4bc96
Gary Hetzel <garyhetzel@gmail.com>
2013-07-19 14:45:10 -0400

    Converted anonymous public paths to a config option

--- plugins/auth/helpers/helpers.rb
+++ plugins/auth/helpers/helpers.rb
@@ -1,17 +1,8 @@
 module App
   module Helpers
     def anonymous?(path)
-      %w{
-        ^/$
-        ^/api/?$
-        ^/api/users/login/?$
-        ^/api/users/machine/?.*$
-        ^/api/provision/?$
-        ^/api/provision/.*/boot/?.*$
-        ^/api/rundeck/nodes/?.*$
-        ^/api/ipxe/boot/?.*$
-      }.each do |p|
-        return true if path =~ Regexp.new(p)
+      App::Config.get('global.authentication.public_paths',[]).each do |p|
+        return true if path =~ Regexp.new("^#{p}$")
       end
 
       return false
